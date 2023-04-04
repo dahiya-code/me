b0ea8ac5214f269b57a8bd8e7d7d95f0d156b093
dahiya-code <d.navya2312@gmail.com>
2023-04-04 13:18:52 +0530

    about, footer, nav

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express=require('express')
 const app=express()
+
 app.listen(3000)
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 app.get('/', (req, res)=>{
-    res.send('working')
+    res.render('index', {title:'Home'})
 })
